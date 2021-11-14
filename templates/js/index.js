1ef987cdb37303c451220c300c77fee2c19167bc
nramos5-ops <78045085+nramos5-ops@users.noreply.github.com>
2021-11-14 16:42:19 -0500

    Added Product Features
    The product feature list is using a javascript that loops through the featured list

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+    $("#slider").bxSlider({
+        auto: true,
+        random: true,
+        captions: true,
+        pager: true,
+        pagerType: 'short',
+        pause: 5000,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20
+    });
 });
